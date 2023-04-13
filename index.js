bb802f973c2a3d5fc4319c441a4c10dc551011cf
Krithik Singh <krithiksingh@gmail.com>
2023-04-13 13:26:25 -0400

    using find lab completed

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+const superbowlWin = (arr) => {
+    if (arr.find(findW)) {
+        return arr.find(findW).year;
+    } else {
+        return undefined;
+    }
+};
+
+const findW = (element) => {
+    return element.result === 'W';
+};
+console.log(superbowlWin());
